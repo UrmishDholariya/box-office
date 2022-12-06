ee35362a5444b55290e29f23abd6581b1b36e5c7
urmish <urmishdholariya777@gmail.com>
2022-12-06 10:07:17 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import Home from './Pages/Home';
+import Starred from './Pages/Starred'
+import Show from './Pages/Show'
+
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-    <Switch>
-      <Route exact path="/">
-        urmish
-      </Route>
-      <Route exact path="/one">
-        rushi
-      </Route>
-      <Route>404 Error</Route>
-    </Switch>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
+        <Route exact path="/show/:id">
+          <Show />
+        </Route>
 
-
+        <Route>
+          <div>Not found</div>
+        </Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
--- src/about.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function about() {
-  return (
-    <div>urmish</div>
-  )
-}
-
-export default about
